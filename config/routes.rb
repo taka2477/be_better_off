1faaa7a527ef009e134aa93192f98314e99186a2
taka2477 <taka2477@gmail.com>
2018-12-10 20:15:03 +0900

    Update posts_controller, routes, sessions/new.html, registrations/edit.html, _header.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root 'home#index'
   get  'home/show'
 
+  resources :posts
+
   devise_for :users, :controllers => {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
     #omniauth_callbacks: 'users/omniauth_callbacks',
   }
 
-  resources :users
+  resources :users, only: [:index, :show]
 end
