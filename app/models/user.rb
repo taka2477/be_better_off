752180e54f81178d785294ddc05d4e7570277fbc
taka2477 <taka2477@gmail.com>
2018-12-20 11:56:50 +0900

    Add accepted culumn to users, Update users/registrations_controller, user, posts/_post_new_form.html, users/registrations/new.html, locales/ja.yml
    users validation追加、利用規約同意実装および日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable # , :omniauthable, omniauth_providers: [:twitter]
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
   validates :age, presence: true, length: { maximum: 2 }
   validates :gender, presence: true, length: { maximum: 2 }
+  validates :accepted, presence: { message: 'を入力してください' }, on: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_065355) do
+ActiveRecord::Schema.define(version: 2018_12_20_022319) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_065355) do
     t.string "username"
     t.integer "age"
     t.string "gender"
+    t.boolean "accepted", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
